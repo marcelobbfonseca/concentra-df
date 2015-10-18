83e7e5c7fdb6133aecc2b5b9a1b37a140f182a89
Cristiano Santos <cristianorsantos.95@gmail.com>
2015-10-17 21:00:19 -0300

    Atualização dos arquivos application.slim e application.sass e virgin.slim apagada

--- db/schema.rb
+++ db/schema.rb
@@ -16,4 +16,21 @@ ActiveRecord::Schema.define(version: 20151011201834) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "jedis", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "padawans", force: :cascade do |t|
+    t.integer  "jedi_id"
+    t.string   "name"
+    t.integer  "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "padawans", ["jedi_id"], name: "index_padawans_on_jedi_id", using: :btree
+
 end
