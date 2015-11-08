8516f241802d9032280f12a38aed234c2c5481da
marcelobbfonseca <tullon_conker@gmail.com>
2015-11-08 11:39:49 +0000

    api do facebook configurado

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :users
+  root to: 'users#index', via: :get
+  get 'auth/facebook', as: "auth_provider"
+  get 'auth/facebook/callback', to: 'users#login'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
