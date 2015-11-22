cd87e23585cc5feff3cabe25bcf7d3ab1e735e14
marcelobbfonseca <tullon_conker@gmail.com>
2015-11-22 17:17:04 +0000

    redirecionamento de home e login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   # GET /users.json
   def index
     @users = User.all
+
   end
 
   # GET /users/1
@@ -64,7 +65,7 @@ class UsersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      @user = User.find(params[:id])
+      @user = User.find(params[:id])  
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
