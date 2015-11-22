9018fce1ab82d8fbfda65b384560c8b219eaf2c6
marcelobbfonseca <tullon_conker@gmail.com>
2015-11-22 01:48:22 +0000

    botao de logout adicionado

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: "test#index"
   
   #devise_for :users, path: "auth", path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
-  devise_scope :user do
+  devise_scope :user do #tava user
     #delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
   end
   
