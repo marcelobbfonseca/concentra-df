e53dcf9d39549d9f7952be8b403db0602ba8d69f
carlosshaka <carlos.shaka02@gmail.com>
2015-12-07 03:28:52 -0200

    Routes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,10 @@
 class HomeController < ApplicationController
     def index
         if !user_signed_in? 
-          redirect_to new_user_registration_path
-        end 
+          redirect_to new_user_session_path
+        else
+          redirect_to new_vacancy_path
+        end
     end
 
 end
