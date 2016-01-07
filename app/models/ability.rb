86aa90835332456073abb3ecab20fb08f5eeb0b9
marcelobbfonseca <marcelobbff@gmail.com>
2016-01-07 19:04:30 -0200

    arrumado erro onde nao logado escolhia quarto

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,14 +3,18 @@ class Ability
 
   def initialize(user)
   		user ||= User.new #guest. deslogado
-
 	  	if user.admin?  #erro #verifica se eh o admin logado
 	  		can :manage, :all
 	   	else			#verifica se eh congressista logado. :create :update :destroy
-	  		can :manage, Vacancy #pode escolher a vaga
-	  		#can :update, User do |user|
-	  		#	this.user == user
+	  		if User.exists?(user) #funciona mas faz query no bd. mais lento
+	  		#if user.encrypted_password # vai verificar se o padrao devise de senha existe pra instancia usuario. Se o bd nao tiver zoado, isso so retorna valor se o cara tiver logado 
+	  			can :manage, Vacancy #pode escolher a vaga
+	  		end	  		
 	  		can :read, :all
+	  	
 	  	end
+
+	  	#se deslogado
+	  	#cannot :read, Vacancy
   end
 end
