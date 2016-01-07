1443051c453cf38088adbcdcf428a005aa00afdd
marcelobbfonseca <marcelobbff@gmail.com>
2016-01-07 18:29:26 -0200

    redirecionamento pra pagina inicial se acesso for nao autorizada

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   	protect_from_forgery with: :exception
   	
-   
+  	#rescue_from CanCan::AccessDenied do |exception|	#quando abrir pagina nao autorizada, volta pra home.
+  	#	redirect_to root_url, :alert => exception.message
+   	#end
 end
